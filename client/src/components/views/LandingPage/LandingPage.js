17044ee7b5c2c448b1a0e5f7b5e2ae66bc6a839a
eunsung ahn <esahn97@gmail.com>
2021-07-13 21:52:47 +0900

    Make product card

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,18 +1,62 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
 import axios from "axios";
+import { Icon, Col, Card, Row } from "antd";
+import Meta from "antd/lib/card/Meta";
 
 function LandingPage() {
+  // DB에 있는 data의 개수만큼 Card를 생성해야 하므로 useState를 사용한다
+  const [Products, setProducts] = useState([]);
+
   useEffect(() => {
     axios.post("/api/product/products").then((response) => {
       if (response.data.success) {
-        console.log(response.data);
+        // console.log(response.data);
+        setProducts(response.data.productInfo);
       } else {
         alert("상품들을 가져오는데 실패했습니다.");
       }
     });
   }, []);
-  return <div>Landing Page</div>;
+
+  const renderCards = Products.map((product, index) => {
+    console.log("product", product);
+
+    return (
+      <Col lg={6} md={8} xs={24} key={index}>
+        <Card
+          cover={
+            <img
+              style={{ width: "100%", maxHeight: "150px" }}
+              src={`http://localhost:5000/${product.images[0]}`}
+            />
+          }
+        >
+          <Meta title={product.title} description={`$${product.price}`} />
+        </Card>
+      </Col>
+    );
+  });
+
+  return (
+    <div style={{ width: "75%", margin: "3rem auto" }}>
+      <div style={{ textAlign: "center" }}>
+        <h2>
+          Let's Travel Anywhere <Icon type="rocket" />
+        </h2>
+      </div>
+
+      {/* Filter */}
+      {/* Search */}
+      {/* Cards */}
+
+      <Row gutter={[16, 16]}>{renderCards}</Row>
+
+      <div style={{ justifyContent: "center" }}>
+        <button>더보기</button>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
